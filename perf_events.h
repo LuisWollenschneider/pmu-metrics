154c63f07c134c91c810a9a0a1d329b3b558973c
Luis Wollenschneider <wollenschneiderluis@gmail.com>
2024-11-05 10:30:25 +0100

    added SKYLAKE.UOPS_RETIRED.RETIRE_SLOTS

--- perf_events.h
+++ perf_events.h
@@ -22,13 +22,15 @@ typedef std::tuple<unsigned long, std::string, std::string> event_name_tuple_t;
  * Map of event IDs to event names.
  * (event_id, event_name, event_description)
  * 
+ * event_id: (EventSel << 8) | Umask
  * event_name format: "ARCHITECTURE.EVENT_NAME"
  * 
  * Referenced from:
  * https://perfmon-events.intel.com/
  */
 const event_name_tuple_t event_names_map[] = {
-	event_name_tuple_t(0xC204, "SKYLAKE.UOPS_RETIRED.MACRO_FUSED", "Uops Retired: Macro-fused")
+	event_name_tuple_t(0xC204, "SKYLAKE.UOPS_RETIRED.MACRO_FUSED", "Uops Retired: Macro-fused"),
+	event_name_tuple_t(0xC202, "SKYLAKE.UOPS_RETIRED.RETIRE_SLOTS", "Uops Retired: Retire Slots"),
 };
 
 const int num_events = sizeof(event_names_map) / sizeof(event_name_tuple_t);
